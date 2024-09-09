b6fb1a92ee8f87d02aa00cec5348ea13c95009ee
Barbara <barbsmok@gmail.com>
2024-09-08 21:52:54 -0300

    ejercicio 3, parte 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 /*
 Cambiando el color de fondo de un elemento de
 HTML al hacerle click
-Base:
-function pintar() {
-  ele.style.backgroundColor = 'yellow';
-}
-const ele = document.getElementById('ele1');
-ele.addEventListener('click', pintar);
+Ejercicio 2.2
 */
-function pintar(event) {
-  event.target.style.backgroundColor = 'yellow';
+// function pintar(event) {
+//   event.target.style.backgroundColor = 'yellow';
+// }
+// const ele = document.getElementById('ele1');
+// ele.addEventListener('click', pintar);
+
+/*
+Pasandole un color como argumento a la
+función pintar
+Ejercicio 2.3
+*/
+function pintar(event, color = 'green') {
+  event.target.style.backgroundColor = color;
 }
 const ele = document.getElementById('ele1');
-ele.addEventListener('click', pintar);
+ele.addEventListener('click', function (event) {
+  pintar(event, 'yellow'); // al hacerle click cambia el color a amarillo
+});
